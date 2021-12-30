219653d48828fe13e41e3bd2eafbd9ee79e357c8
Sergei Gordeichuk <gordeychuk.s@gmail.com>
2021-12-30 14:30:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify, Auth from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
     <>
       <h1>Hello {user.username}</h1>
-      <div>Token: {user.access_token}</div>
       <button onClick={signOut}>Sign out</button>
     </>
   );
