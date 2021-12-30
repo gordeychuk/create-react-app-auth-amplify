957f10a620da74271233543dc96361a7d1fb462d
Sergei Gordeichuk <gordeychuk.s@gmail.com>
2021-12-30 14:38:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Amplify } from 'aws-amplify';
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import './App.css';
+// import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
