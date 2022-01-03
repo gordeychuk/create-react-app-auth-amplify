6d6f3c8cb17d05adccadb89e4d041a106517c3fc
Sergey Gordeychuk <sergey.gordeychuk@what.digital>
2022-01-03 19:50:12 +0300

    Implementing custom sign in

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-import {Amplify} from 'aws-amplify';
+import {Amplify, Auth} from 'aws-amplify';
 
-import {withAuthenticator} from '@aws-amplify/ui-react';
+import {Authenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 
-console.log(awsExports);
 Amplify.configure(awsExports);
 
-function App({signIn, signOut, user}) {
-    console.log(user);
+export default function App() {
+    const services = {
+        async handleSignIn(formData) {
+            let {username, password} = formData;
 
+            console.log('custom sign in');
+            return Auth.signIn({
+                username,
+                password,
+            });
+        },
+    };
     return (
-        <>
-            <h1>Hello {user && user.username}</h1>
-            <p>access token</p>
-            <textarea rows={4}>
+        <Authenticator services={services}>
+            {({signOut, user}) => (
+                <>
+                    <h1>Hello {user && user.username}</h1>
+                    <p>access token</p>
+                    <textarea rows={4}>
                 {user && user.signInUserSession.accessToken.jwtToken}
             </textarea>
-            <button onClick={() => {
-                navigator.clipboard.writeText(user.signInUserSession.accessToken.jwtToken).then(function () {
-                    console.log('Async: Copying to clipboard was successful!');
-                }, function (err) {
-                    console.error('Async: Could not copy text: ', err);
-                });
-            }}>Copy to clipboard
-            </button>
-            <p>idToken</p>
-            <textarea rows={4}>
+                    <button onClick={() => {
+                        navigator.clipboard.writeText(user.signInUserSession.accessToken.jwtToken).then(function () {
+                            console.log('Async: Copying to clipboard was successful!');
+                        }, function (err) {
+                            console.error('Async: Could not copy text: ', err);
+                        });
+                    }}>Copy to clipboard
+                    </button>
+                    <p>idToken</p>
+                    <textarea rows={4}>
                 {user && user.signInUserSession.idToken.jwtToken}
             </textarea>
-            <button onClick={() => {
-                navigator.clipboard.writeText(user.signInUserSession.idToken.jwtToken).then(function () {
-                    console.log('Async: Copying to clipboard was successful!');
-                }, function (err) {
-                    console.error('Async: Could not copy text: ', err);
-                });
-            }}>Copy to clipboard
-            </button>
+                    <button onClick={() => {
+                        navigator.clipboard.writeText(user.signInUserSession.idToken.jwtToken).then(function () {
+                            console.log('Async: Copying to clipboard was successful!');
+                        }, function (err) {
+                            console.error('Async: Could not copy text: ', err);
+                        });
+                    }}>Copy to clipboard
+                    </button>
 
-            <button onClick={signOut}>Sign out</button>
-        </>
+                    <button onClick={signOut}>Sign out</button>
+                </>
+
+            )¬}
+
+        </Authenticator>
     );
 }
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
